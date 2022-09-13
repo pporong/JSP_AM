a6edd9860a9bc6b4c6b5071c7e848facb165fac3
pporong <inane09@gmail.com>
2022-09-13 16:35:57 +0900

    구구단, 파라미터 기초

--- src/main/java/com/KoreaIT/java/am/HomeMainServlet2.java
+++ src/main/java/com/KoreaIT/java/am/HomeMainServlet2.java
@@ -11,13 +11,28 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/home/printDan")
 public class HomeMainServlet2 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("< 9단 > <br>");
+		
 		response.setContentType("text/html; charset=UTF-8");
 		
+		// 파라미터 선언
+		String inputedDan = request.getParameter("dan");
+		if(inputedDan == null) {
+			inputedDan = "1";
+		}
 		
-		int dan = 9;
-		for(int i = 1; i <= 9; i++) {
+		String inputedLimit = request.getParameter("limit");
+		if(inputedLimit == null) {
+			inputedLimit = "9";
+		}
+		
+		// 파라미터를 정수화
+		int dan = Integer.parseInt(inputedDan);
+		int limit = Integer.parseInt(inputedLimit);
+
+		response.getWriter().append(String.format(" %d단 <br />", dan));
+		for(int i = 1; i <= limit; i++) {
 			response.getWriter().append(String.format(" %d X %d = %d <br>", dan, i, dan * i));
 		}
+		
 	}
 }
